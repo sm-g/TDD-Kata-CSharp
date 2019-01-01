58fe0388c0a81b327e21cb11af479f2290a56bf5
smg <semagerasimov@gmail.com>
2019-01-01 15:30:52 +0300

    Add project files
    completed http://osherove.com/tdd-kata-1/ at number 7)

---
+++ TDDKata/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDKata
+{
+    /// <summary>
+    /// http://osherove.com/tdd-kata-1/
+    /// </summary>
+    public class Calculator
+    {
+        private const char Comma = ',';
+        private const char NewLine = '\n';
+
+        public static object Add(string input)
+        {
+            var (separatedNumbers, delimiters) = ParseDelimiters(input);
+
+            var numbers = separatedNumbers
+                .Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .Where(x => x <= 1000)
+                .ToList();
+
+            CheckNegatives(numbers);
+
+            return numbers.Sum();
+        }
+
+        private static void CheckNegatives(IEnumerable<int> numbers)
+        {
+            var negatives = numbers.Where(x => x < 0);
+            if (negatives.Any())
+                throw new ArgumentException("negatives not allowed " + string.Join(",", negatives));
+        }
+
+        private static (string separatedNumbers, string[] delimiters) ParseDelimiters(string input)
+        {
+            if (input.StartsWith("//"))
+            {
+                if (input[2] == '[')
+                {
+                    // multi char delimiter
+                    var endOfDelimSequence = input.IndexOf("]\n");
+                    var delimiterSequence = input.Substring(3, endOfDelimSequence - 3);
+                    return (input.Substring(endOfDelimSequence + 2), new[] { delimiterSequence, NewLine.ToString() });
+                }
+                return (input.Substring(4), new[] { input[2].ToString(), NewLine.ToString() });
+            }
+            return (input, new[] { Comma.ToString(), NewLine.ToString() });
+        }
+    }
+}
---
+++ TDDKata/CalculatorTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDKata
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_EmptyString_ShouldReturnZero()
+        {
+            var result = Calculator.Add("");
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Add_OneNumber_ShouldReturnThatNumber()
+        {
+            var result = Calculator.Add("3");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestCase("3,4", 7)]
+        [TestCase("3,5,4,100", 112)]
+        public void Add_AnyCountOfNumbers_ShouldReturnSum(string input, int expected)
+        {
+            var result = Calculator.Add(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_WithNewLinesBetweenNumbers_ShouldReturnSum()
+        {
+            var result = Calculator.Add("3\n4,5,1");
+            Assert.AreEqual(13, result);
+        }
+
+        [Test]
+        public void Add_WithDelimeter_ShouldReturnSum()
+        {
+            var result = Calculator.Add("//;\n4;1");
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Add_WithDelimeterAndNewLinesBetweenNumbers_ShouldReturnSum()
+        {
+            var result = Calculator.Add("//;\n4;1\n3;1");
+            Assert.AreEqual(9, result);
+        }
+
+        [Test]
+        public void Add_Negative_ShouldThrow()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => Calculator.Add("-1"));
+            Assert.That(ex.Message, Contains.Substring("negatives not allowed").IgnoreCase);
+        }
+
+        [Test]
+        public void Add_WithNegativeNumbers_ShouldReturnThrowAndListNegatives()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => Calculator.Add("-1,5,-1"));
+            Assert.That(ex.Message, Contains.Substring("negatives not allowed -1,-1").IgnoreCase);
+        }
+
+        [TestCase("3,999", 1002)]
+        [TestCase("3,1000", 1003)]
+        [TestCase("3,1001", 3)]
+        [TestCase("1001", 0)]
+        [TestCase("1003,800,3000", 800)]
+        public void Add_ShouldNotAccountNumbersBiggerThan1000(string unput, int expected)
+        {
+            var result = Calculator.Add(unput);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("//[***]\n1***2***3", 6)]
+        [TestCase("//[]]]\n1]]2]]3", 6)]
+        [TestCase("//[]]]\n1]]2\n3", 6)]
+        [TestCase("//[*]\n1*2*3", 6)]
+        public void Add_WithMultiCharDelimeter_ShouldReturnSum(string unput, int expected)
+        {
+            var result = Calculator.Add(unput);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
